7198e867bfbcb3536085af08ae22f9ff88201ce7
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-05-19 16:41:01 -0400

    changed hue colors js file

--- chamelon.js
+++ chamelon.js
@@ -4,64 +4,32 @@ const hueTitle = yarnContainer.querySelector('#HueTitle');
 const yarn_colors = yarnContainer.querySelectorAll('.yarn-changing-image');
 
 let ChangedColor = false;
-const changeColors = () => {
-    ChangedColor = true;
 
-    hueTitle.style.opacity = 0;
+yarn_colors.forEach((yrnImg) => {
+    yrnImg.addEventListener('mouseenter', () => {
+            const yarnBall = yrnImg.querySelector('img');
 
-    //    Change the colors of the title
-    setTimeout(() => {
-        hueTitle.innerHTML = 'Hues with Sun';
-        hueTitle.style.opacity = 1;
-        hueTitle.style.color = 'rgb(220, 176, 0)';
-    }, 1000);
+                    const originalImage = yarnBall.src;
+                    const newImage = yarnBall.dataset.src;
+            
+                    yrnImg.style.opacity = 0;
+                    setTimeout(() => {
+                        yarnBall.src = newImage;
+                        yarnBall.dataset.src = originalImage;
+                        yrnImg.style.opacity = 1;
+                    }, 00)
+    })
+    yrnImg.addEventListener('mouseleave', () => {
+        const yarnBall = yrnImg.querySelector('img');
 
-    //    Change the colors of the yarns
-    changeYarnColors();
-}
-
-const changeYarnColors = () => {
-    yarn_colors.forEach((colors)=> {
-        const yarnBall = colors.querySelector('img');
-
-        const originalImage = yarnBall.src;
-        const newImage = yarnBall.dataset.src;
-
-        colors.style.opacity = 0;
-        setTimeout(() => {
-            yarnBall.src = newImage;
-            yarnBall.dataset.src = originalImage;
-            colors.style.opacity = 1;
-            hueButton.removeAttribute('disabled');
-        }, 1500)
-     })
-}
-const unChangeColors = () => {
-    ChangedColor = false;
-
-    hueTitle.style.opacity = 0;
-
-    //    Change the colors of the title
-    setTimeout(() => {
-        hueTitle.innerHTML = 'Hues without Color';
-        hueTitle.style.opacity = 1;
-        hueTitle.style.color = 'black';
-    }, 1000);
-    //    Change the colors of the yarns
-    changeYarnColors();
-}
-
- // add Button to active change colors
-const ChangeClick = () => {
-
-        hueButton.setAttribute('disabled', 'disabled');
-
-        switch (ChangedColor) {
-            case false: changeColors();
-                break;
-            case true: unChangeColors();
-                break;
-        }
-} 
-
-hueButton.addEventListener('click', ChangeClick)
+                const originalImage = yarnBall.src;
+                const newImage = yarnBall.dataset.src;
+        
+                yrnImg.style.opacity = 0;
+                setTimeout(() => {
+                    yarnBall.src = newImage;
+                    yarnBall.dataset.src = originalImage;
+                    yrnImg.style.opacity = 1;
+                }, 00)
+    })
+})
